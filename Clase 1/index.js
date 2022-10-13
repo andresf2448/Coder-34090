e57af4c2d65bc33d4bce23024a923bf5755655ec
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-10-13 09:46:48 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,44 @@
+// comentario de una sola linea
+/* 
+comentario de
+bloque
+*/
+// para el nombre de las variables utilizamos camel case
+// var nombre = "andres"; //no recomendado
+// let edad = 5; //recomendado
+
+// nombreBonitoGrande
+
+// const NUMERO = 5;
+// NUMERO = 10;
+
+// const EDAD = 5;
+// console.log(EDAD);
+// let numero = "5";
+
+// console.log(typeof(numero));
+
+/* let numeroUno = 2;
+let numeroDos = 5;
+
+let resultadoSuma = numeroUno + numeroDos;
+let resultadoMultiplicacion = numeroUno * numeroDos;
+
+console.log("este es el resultado de la suma", resultadoSuma);
+console.log("este es el resultado de la multiplicacion", resultadoMultiplicacion); */
+
+// let nombre = "Camila";
+// let apellido = "Ruiz";
+// let edad = 10;
+
+// let nombreCompleto = nombre + " " + apellido;
+// let nombreCompleto = `${nombre} ${apellido} tu edad es ${edad}`;
+// console.log(nombreCompleto);
+
+//PROMPT
+// let nombre = prompt("Ingrese su nombre");
+// alert(nombre);
+
+let numero1 = prompt("Ingrese numero1");
+let numero2 = prompt("Ingrese numero2");
+console.log(Number(numero1) + parseInt(numero2));
