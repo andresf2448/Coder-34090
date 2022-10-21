c5107361f71d016c38de285a50db684ff8045ad9
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-10-21 09:29:12 -0500

    After 1

---
+++ After 1/index.js
@@ -0,0 +1,31 @@
+// console.log(typeof("andres"));
+// console.log(typeof(parseInt("1")));
+
+/* let numero1 = parseInt(prompt("Ingrese el numero 1"));
+let numero2 = parseInt(prompt("Ingrese el numero 2"));
+console.log(typeof(numero1));
+console.log(typeof(numero2));
+let resultado = numero1 + numero2;
+
+alert(resultado); */
+
+/* let email = prompt("ingrese el email");
+let pasword = prompt("ingrese la contraseña");
+
+if (email === "markitox@gmail.com") {
+  if (pasword === "milanesa") {
+    alert("bienvenidos");
+  } else {
+    alert("pasword malo");
+  }
+} else {
+  alert("email incorrecto");
+} */
+
+let nombre = prompt("Ingrese su nombre");
+
+if(!nombre){
+  alert("Bienvenido");
+}else{
+  alert("No ingresaste nada");
+}
